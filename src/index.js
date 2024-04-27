01548746227a4b78b0f2eb1d85b479f40932d858
gollldddd <viettriswiitt@gmail.com>
2024-04-27 14:13:06 +1000

    change link

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import Projects from './pages/Projects';
 const router=createBrowserRouter([
   {path: '/',
   element: <Layout/>,
-  errorElement:<UserProfile/>,
+  errorElement:<Layout/>,
   children: [
     {index:true, element:<Home />},
     {path: "posts/:id", element: <PostDetail/>},
